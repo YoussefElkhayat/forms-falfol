0239224eec47a4204874996b8f6f9d5164b2bc6c
YoussefElkhayat <youssefelkhayat43@gmail.com>
2026-05-07 21:48:55 +0300

    Add files via upload

---
+++ WinFormsApp8/WinFormsApp8/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WinFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            pass = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(0, 0, 0, 1);
+            label1.Font = new Font("Showcard Gothic", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(392, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(155, 54);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            label1.Click += label1_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(0, 0, 0, 1);
+            label2.Font = new Font("Showcard Gothic", 22.2F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(356, 132);
+            label2.Name = "label2";
+            label2.Size = new Size(225, 46);
+            label2.TabIndex = 1;
+            label2.Text = "User Name";
+            label2.Click += label2_Click;
+            // 
+            // pass
+            // 
+            pass.AutoSize = true;
+            pass.BackColor = Color.FromArgb(0, 0, 0, 1);
+            pass.Font = new Font("Showcard Gothic", 22.2F, FontStyle.Bold);
+            pass.ForeColor = Color.White;
+            pass.Location = new Point(356, 267);
+            pass.Name = "pass";
+            pass.Size = new Size(229, 46);
+            pass.TabIndex = 2;
+            pass.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Cursor = Cursors.Hand;
+            textBox1.Location = new Point(289, 181);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(374, 43);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Cursor = Cursors.Hand;
+            textBox2.Location = new Point(289, 326);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(374, 43);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(392, 404);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 55);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.TextAlign = ContentAlignment.TopCenter;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(0, 0, 0, 1);
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(289, 372);
+            label3.Name = "label3";
+            label3.Size = new Size(273, 25);
+            label3.TabIndex = 6;
+            label3.Text = "invalid password or username!";
+            label3.Visible = false;
+            label3.Click += label3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(16F, 37F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(943, 504);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pass);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(6);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "مطعم فلفول ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label pass;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label3;
+    }
+}
---
+++ WinFormsApp8/WinFormsApp8/Form2.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFormsApp8
+{
+    public partial class Form2 : Form
+    {
+        List<Fooditem> orders = new List<Fooditem>();
+        double total = 0;
+        int count = 1;
+        public Form2()
+        {
+            InitializeComponent();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        }
+
+
+        public void label(string Name, double price, int Calories)
+        {
+            label1.Text = $"المواصفات:\n تم اضافة {Name}\n السعر : {price} \n يحتوي على {Calories} سعرة حراري";
+            listBox1.Items.Add($"{Name} - {price}ج");
+
+        }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button22_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            orders.Clear();
+            total = total * 0;
+            label2.Text = $"الإجمالي: {total} جنيه";
+            count++;
+            label4.Text = $"{count}";
+            label1.Text = " ";
+
+        }
+
+        // menu ####
+        private void panelmenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        void clearmenu()
+        {
+            panelmenu.Controls.Clear();
+            y = 10;
+        }
+        int y = 10;
+        void creatbtn(string name, double price, int calories)
+        {
+            Button btn = new Button();
+            btn.Text = name;
+            btn.Width = 200;
+            btn.Height = 90;
+            btn.Top = y;
+
+            btn.Left = 100;
+            y += 60;
+            btn.Click += (s, e) =>
+            {
+                var existing = orders.FirstOrDefault(x => x.Name == name);
+                label(name, price, calories);
+
+                if (existing != null)
+                {
+                    existing.Quantity++;
+                }
+                else
+                {
+                    orders.Add(new Fooditem
+                    {
+                        Name = name,
+                        price = price,
+                        Calories = calories,
+                        Quantity = 1
+
+
+                    });
+                }
+
+                RefreshCart();
+
+            };
+            panelmenu.Controls.Add(btn);
+
+        }
+
+
+        void RefreshCart()
+        {
+            listBox1.Items.Clear();
+
+            total = 0;
+
+            foreach (var item in orders)
+            {
+                double itemTotal = item.price * item.Quantity;
+
+                listBox1.Items.Add($"{item.Name}  = {itemTotal}ج    x{item.Quantity}       يحتوي على {item.Calories} سعرة للقطعة الواحدة");
+
+                total += itemTotal;
+            }
+
+            label2.Text = $"الإجمالي: {total} جنيه";
+        }
+        void RemoveItem(string name)
+        {
+            var item = orders.FirstOrDefault(x => x.Name == name);
+
+            if (item != null)
+            {
+                orders.Remove(item);
+                RefreshCart();
+            }
+        }
+        private void btnpizza_Click(object sender, EventArgs e)
+        {
+            clearmenu();
+            creatbtn("بيتزا فراخ", 90, 800);
+            creatbtn("بيتزا باربكيو فراخ", 100, 900);
+            creatbtn("بيتزا سبايسي فراخ", 100, 830);
+            creatbtn("بيتزا رانش فراخ", 105, 860);
+            creatbtn("بيتزا بيبروني", 95, 850);
+            creatbtn("بيتزا ميكس لحوم", 130, 1100);
+            creatbtn("بيتزا سجق", 95, 870);
+            creatbtn("بيتزا مارجريتا", 70, 650);
+            creatbtn("بيتزا تشيز سوبريم", 85, 780);
+            creatbtn("بيتزا فور تشيز", 110, 950);
+            creatbtn("بيتزا رومي مدخن", 100, 880);
+            creatbtn("بيتزا خضار", 75, 600);
+            creatbtn("بيتزا مشروم", 80, 620);
+            creatbtn("بيتزا سي فود", 140, 750);
+
+
+            creatbtn("بيتزا هاواي", 95, 820);
+        }
+        private void btncreb_Click(object sender, EventArgs e)
+        {
+            clearmenu();
+            creatbtn("كريب بانيه", 120, 500);
+            creatbtn("كريب كرسبي", 125, 600);
+            creatbtn("كريب شيش طاووق", 120, 520);
+            creatbtn("كريب فاهيتا فراخ", 130, 550);
+            creatbtn("كريب فراخ بالمشروم", 128, 560);
+
+            creatbtn("كريب كفتة", 135, 520);
+            creatbtn("كريب سجق", 130, 600);
+            creatbtn("كريب برجر", 150, 650);
+            creatbtn("كريب ميكس لحوم", 145, 750);
+
+            creatbtn("كريب جبنة موتزاريلا", 127, 450);
+            creatbtn("كريب جبنة رومي", 100, 470);
+            creatbtn("كريب ميكس جبن", 100, 550);
+
+            creatbtn("كريب نوتيلا", 130, 650);
+            creatbtn("كريب نوتيلا موز", 135, 700);
+            creatbtn("كريب شوكولاتة", 125, 600);
+            creatbtn("كريب ميكس شوكولاتة", 150, 850);
+            creatbtn("كريب تيتانك", 130, 560);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clearmenu();
+
+
+            creatbtn("برجر كلاسيك لحم", 85, 750);
+            creatbtn("تشيز برجر", 95, 820);
+            creatbtn("دبل تشيز برجر", 130, 1100);
+            creatbtn("تريبل برجر لحم", 160, 1350);
+            creatbtn("ستيك هاوس برجر", 120, 950);
+
+
+            creatbtn("برجر فراخ", 80, 700);
+            creatbtn("تشيز تشيكن برجر", 95, 800);
+            creatbtn("سبايسي تشيكن برجر", 90, 760);
+            creatbtn("رانش تشيكن برجر", 100, 820);
+            creatbtn("كريسبي تشيكن برجر", 105, 850);
+            creatbtn("باربكيو تشيكن برجر", 110, 880);
+
+
+            creatbtn("مشروم سويس برجر", 115, 950);
+            creatbtn("بلو تشيز برجر", 120, 980);
+            creatbtn("باربكيو بيف برجر", 105, 880);
+            creatbtn("هاني ماسترد برجر", 100, 830);
+
+            creatbtn("برجر بيكون لحم", 120, 1000);
+            creatbtn("برجر بيكون فراخ", 115, 950);
+            creatbtn("برجر ميكس لحوم", 140, 1150);
+            creatbtn("مونستر برجر (دبل لحم + فراخ)", 170, 1500);
+            creatbtn("فيجي برجر", 85, 650);
+            creatbtn("فلافل برجر", 70, 600);
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("اختر عنصر للحذف");
+                return;
+            }
+
+            string selected = listBox1.SelectedItem.ToString();
+
+            var item = orders.FirstOrDefault(x => selected.Contains(x.Name));
+
+            if (item != null)
+            {
+                orders.Remove(item);
+                RefreshCart();
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbldatatime.Text = DateTime.Now.ToString("dd/MM/yyyy - hh:mm:ss tt");
+        }
+
+
+    }
+}
